9ef0b1c04afa690ba1a51e6a875b5f2b04478de3
Sergeeva Larisa <mindzu@mail.ru>
2024-06-05 11:53:16 +0500

    step 3 complete

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -26,7 +26,8 @@ def urls():
     errors = validate_url(url)
     if errors:
         flash(errors[0], 'alert-danger')
-        return redirect(url_for('main'))
+        msg = get_flashed_messages(with_categories=True)
+        return render_template('index.html', messages=msg)
     o = urlparse(url)
     normalized_url = str(urlunparse([o.scheme, o.hostname, '', '', '', '']))
     page_id = get_id(normalized_url)
@@ -41,7 +42,7 @@ def urls():
 
 @app.get('/urls')
 def urls_get():
-    cur.execute ('SELECT * FROM urls;')
+    cur.execute ('SELECT * FROM urls ORDER BY created_at DESC, name ASC;')
     msg = get_flashed_messages(with_categories=True)
     return render_template('all.html', messages=msg, rows=cur.fetchall()) 
 
@@ -49,7 +50,12 @@ def urls_get():
 @app.get('/urls/<int:id>')
 def url_id(id):
     msg = get_flashed_messages(with_categories=True)
-    return render_template('edit_page.html', messages=msg) 
+    data = get_data(id)
+    return render_template('edit_page.html',
+                           messages=msg,
+                           id=id,
+                           name=data[1],
+                           created_at=data[2])
 
 
 def validate_url(data):
@@ -65,7 +71,7 @@ def validate_url(data):
 
 def get_id(data):
     cur.execute("SELECT * FROM urls WHERE name=%s;", (data,))
-    if cur.rowcount > 0:  # .fetchone() is not None:
+    if cur.rowcount > 0:
         return cur.fetchone()[0]
     return None
 
@@ -74,6 +80,11 @@ def add_to_db(url, created_at):
     cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s);", (url, created_at))
     conn.commit()
     return get_id(url)
+
+
+def get_data(id):
+    cur.execute('SELECT * FROM urls WHERE id=%s ORDER BY created_at DESC, name ASC;', (id,))
+    return cur.fetchone()
 
 # validators = "^0.20.0"
 # requests = "^2.31.0"
