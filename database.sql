59c616598d917bd5504609e9276dd6f0a7c0bbc5
Sergeeva Larisa <mindzu@mail.ru>
2024-06-06 12:29:10 +0500

    step 4 complete

--- database.sql
+++ database.sql
@@ -5,3 +5,13 @@ CREATE TABLE urls (
     name VARCHAR(255),
     created_at DATE
 );
+
+CREATE TABLE url_checks (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id BIGINT,
+    status_code int,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description VARCHAR(255),
+    created_at DATE
+);
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -13,6 +13,9 @@ DB_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DB_URL)
 cur = conn.cursor()
 
+FLASH_EXIST = 'Страница уже существует'
+FLASH_ADDED = 'Страница успешно добавлена'
+FLASH_CHECKED = 'Страница успешно проверена'
 
 @app.route('/')
 def main():
@@ -32,17 +35,24 @@ def urls():
     normalized_url = str(urlunparse([o.scheme, o.hostname, '', '', '', '']))
     page_id = get_id(normalized_url)
     if page_id:
-        flash('Страница уже существует', 'alert-info')
+        flash(FLASH_EXIST, 'alert-info')
         return redirect(url_for('url_id', id=page_id))
     today = datetime.datetime.now().date()
     page_id = add_to_db(normalized_url, today)
-    flash('Страница успешно добавлена'+ normalized_url, 'alert-success')
+    flash(FLASH_ADDED, 'alert-success')
     return redirect(url_for('url_id', id=page_id))
 
 
 @app.get('/urls')
 def urls_get():
-    cur.execute ('SELECT * FROM urls ORDER BY created_at DESC, name ASC;')
+    cur.execute ('''SELECT urls.id,
+                    urls.name,
+                    url_checks.created_at,
+                    status_code
+                 FROM urls 
+                 LEFT JOIN url_checks
+                 ON urls.id = url_checks.url_id
+                 ORDER BY url_checks.created_at DESC NULLS LAST, name ASC;''')
     msg = get_flashed_messages(with_categories=True)
     return render_template('all.html', messages=msg, rows=cur.fetchall()) 
 
@@ -51,11 +61,20 @@ def urls_get():
 def url_id(id):
     msg = get_flashed_messages(with_categories=True)
     data = get_data(id)
+    checks = get_checks(id)
     return render_template('edit_page.html',
                            messages=msg,
                            id=id,
                            name=data[1],
-                           created_at=data[2])
+                           created_at=data[2],
+                           rows = checks)
+
+
+@app.post('/urls/<int:id>/checks')
+def check_url(id):
+    add_new_check(id)
+    flash(FLASH_CHECKED, 'alert-info')
+    return redirect(url_for('url_id', id=id), 302)
 
 
 def validate_url(data):
@@ -85,6 +104,27 @@ def add_to_db(url, created_at):
 def get_data(id):
     cur.execute('SELECT * FROM urls WHERE id=%s ORDER BY created_at DESC, name ASC;', (id,))
     return cur.fetchone()
+
+
+def add_new_check(id):
+    cur.execute('INSERT INTO url_checks (url_id, created_at) VALUES (%s, %s);', 
+                (id, datetime.datetime.now().date()))
+    conn.commit()
+    return
+
+
+def get_checks(id):
+    cur.execute("""SELECT url_checks.id,
+                    status_code,
+                    h1,
+                    title,
+                    description,
+                    url_checks.created_at
+                FROM url_checks
+                INNER JOIN urls
+                ON urls.id = url_checks.url_id
+                WHERE urls.id = %s;""", (id,))
+    return cur.fetchall()
 
 # validators = "^0.20.0"
 # requests = "^2.31.0"
