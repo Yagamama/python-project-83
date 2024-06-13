c8803fecfa102e61bc7bd9117bc516d584fed259
Sergeeva Larisa <mindzu@mail.ru>
2024-06-14 00:19:07 +0500

    edit __init__.py

--- database.sql
+++ database.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS urls, url_checks;
+DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
 
 CREATE TABLE urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -8,7 +9,7 @@ CREATE TABLE urls (
 
 CREATE TABLE url_checks (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id BIGINT,
+    url_id BIGINT REFERENCES urls (id) ON DELETE CASCADE,
     status_code int,
     h1 VARCHAR(255),
     title VARCHAR(255),
